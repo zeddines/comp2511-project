a32efb46daf797989a941306d9bf607214e0e782
Gomsi <gomsi1020@gmail.com>
2021-10-30 22:55:40 +1100

    Commit #2 (Updated Group names in the frontend)

--- src/test/java/dungeonmania/MovementTest.java
+++ src/test/java/dungeonmania/MovementTest.java
@@ -1,4 +1,26 @@
 package dungeonmania;
 
+import dungeonmania.*;
+import dungeonmania.entity.creature.Player;
+import dungeonmania.entity.square.Boulder;
+import dungeonmania.game.Game;
+import dungeonmania.map.DungeonMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.response.models.DungeonResponse;
+import java.util.List;
+import dungeonmania.response.models.*;
+import dungeonmania.util.*;
+
+
 public class MovementTest {
+    @Test
+    public void simpleMove() {
+        DungeonManiaController newController = new DungeonManiaController();
+//        DungeonResponse newResponse = newController.newGame()
+    }
 }
