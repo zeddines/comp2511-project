fe09d1148c65fead77f6171dd2aa0ca840fc7a76
Alan Vo <z5164807@ad.unsw.edu.au>
2021-11-10 22:16:39 +1100

    Gotta work on my own branch not here

--- src/test/java/dungeonmania/DoorTest.java
+++ src/test/java/dungeonmania/DoorTest.java
@@ -16,7 +16,20 @@ public class DoorTest {
     @Test
     // door acts like wall (no key)
     public void testDoorNoKey() {
-        return; 
+        DungeonManiaController newController = new DungeonManiaController();
+        DungeonResponse newResponse = newController.newGame("advanced-2", "Peaceful");
+        List<EntityResponse>  newEntities = newResponse.getEntities();
+
+        Position newDoor = new Position(15,9);
+
+        newEntities.stream()
+                .filter(e -> e.getType().equals("player"))
+                .forEach(e -> assertEquals(e.getPosition(), original));
+
+
+        DungeonResponse Response = newController.tick("player", Direction.RIGHT);
+        
+
     }
 
     // test for door-key interaction can go in items tests
