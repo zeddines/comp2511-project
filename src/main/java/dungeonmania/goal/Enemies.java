e202c8f13cbf2abdf6ecb0c5297d1b7a2fff0392
Gomsi <gomsi1020@gmail.com>
2021-11-13 00:12:44 +1100

    Commit #4 (Enemies)

---
+++ src/main/java/dungeonmania/goal/Enemies.java
@@ -0,0 +1,19 @@
+package dungeonmania.goal;
+
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Enemies implements Goals{
+    public Enemies() {
+    }
+
+    @Override
+    public String goalSatisfied(DungeonResponse d) {
+        // TODO: PLEASE FINISH
+        return null;
+    }
+}
